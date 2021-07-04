1f14f99f8b823596c8eeeeb868cddae9e9df52f0
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-07-04 11:24:45 +0530

    Create dev.js

---
+++ plugins/dev.js
@@ -0,0 +1,8 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'creat', fromMe: false}, (async (message, match) => {
+
+    await message.sendMessage("Create bu RAVANA-sl @github\n```https://github.com/RAVANA-SL```\n*💫ŖĄŴÑÆ*");
+
+}));
